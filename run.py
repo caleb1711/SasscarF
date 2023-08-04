9afa26a60012b75268141af4013ede7e7030ffc9
caleb1711 <>
2023-08-04 13:40:09 +0000

    create while loop to determin when a Player wins, looses or ties a game

--- run.py
+++ run.py
@@ -1,25 +1,32 @@
 import random
 
 options = ("rock", "paper", "scissors")
-player = None
-computer = random.choice(options)
+running = True
 
-while player not in options: 
-             player = input("Enter a choice (rock, paper, scissors): ")
 
-print(f"Player: {player}")
-print(f"Computer: {computer}")
+while running:
+    player = None
+    computer = random.choice(options)
 
-if player == computer:
+    while player not in options: 
+                player = input("Enter a choice (rock, paper, scissors): ")
+
+    print(f"Player: {player}")
+    print(f"Computer: {computer}")
+
+    if player == computer:
         print("It's a tie!")
-elif player == "rock" and computer == "scissors":
+    elif player == "rock" and computer == "scissors":
         print("You win!")
-#     elif player == "paper" and computer == "rock":
-#         print("You win!")
-#     elif player == "scissors" and computer == "paper":
-#         print("You win!")
-#     else:
-#         print("You lose!")
+    elif player == "paper" and computer == "rock":
+        print("You win!")
+    elif player == "scissors" and computer == "paper":
+        print("You win!")
+    else:
+        print("You lose!")
+        if not input("play again? (y/n): ").lower == "y":
+            running = False
+        print("Thanks for playing!")    
 
 
            
