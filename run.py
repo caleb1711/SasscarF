e0547569acf5ed0b2e06dabe328916f8e0e1e9c5
caleb1711 <>
2023-08-04 13:09:11 +0000

    set the options, the player's and the computer's choice

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+options = ("rock", "paper", "scissors")
+player = None
+computer = random.choice(options)
+
+while player not in options: 
+             player = input("Enter a choice (rock, paper, scissors): ")
+
+print(f"Player: {player}")
+print(f"Computer: {computer}")
+
+if player == computer:
+        print("It's a tie!")
+elif player == "rock" and computer == "scissors":
+        print("You win!")
+#     elif player == "paper" and computer == "rock":
+#         print("You win!")
+#     elif player == "scissors" and computer == "paper":
+#         print("You win!")
+#     else:
+#         print("You lose!")
+
+
+           
+                
+            
