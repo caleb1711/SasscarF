2bc96c992efd9621a562f7af01a8bdd048bde6c8
caleb1711 <>
2023-08-06 15:08:30 +0000

    set the game options to play again or quit playing

--- run.py
+++ run.py
@@ -3,32 +3,34 @@ import random
 options = ("rock", "paper", "scissors")
 running = True
 
-
 while running:
-    player = None
-    computer = random.choice(options)
 
-    while player not in options: 
-                player = input("Enter a choice (rock, paper, scissors): ")
+        player = None
+        computer = random.choice(options)
 
-    print(f"Player: {player}")
-    print(f"Computer: {computer}")
+        
+        while player not in options: 
+            player = input("Enter a choice (rock, paper, scissors): ")
 
-    if player == computer:
-        print("It's a tie!")
-    elif player == "rock" and computer == "scissors":
-        print("You win!")
-    elif player == "paper" and computer == "rock":
-        print("You win!")
-    elif player == "scissors" and computer == "paper":
-        print("You win!")
-    else:
-        print("You lose!")
-        if not input("play again? (y/n): ").lower == "y":
+        print(f"player: {player}")
+        print(f"computer: {computer}")
+
+        if player == computer:
+            print("It's a tie!")
+        elif player == "rock" and computer == "scissors":
+            print("You WIN!")
+        elif player == "paper" and computer == "rock":
+            print("You WIN!")   
+        elif player == "scissors" and computer == "paper":
+            print("You WIN!")
+
+        else:
+            print("You Lost!")
+                
+        if not input("Play again? (y/n): ").lower() == "y":
             running = False
-        print("Thanks for playing!")    
+            
+print("Thanks for playing!")
 
-
-           
                 
             
